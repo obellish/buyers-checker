ec8cb8a639cc535615c48ff8e31a52f898183255
obellish <obellish@mpilabels.com>
2024-03-14 08:49:02 -0400

    more work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::atomic::{AtomicUsize, Ordering::SeqCst};
+
+use miette::{IntoDiagnostic as _, Result};
+use tokio::runtime::Builder;
+
+static THREAD_ID: AtomicUsize = AtomicUsize::new(1);
+
+fn main() -> Result<()> {
+	Builder::new_multi_thread()
+		.thread_name_fn(|| {
+			let id = THREAD_ID.fetch_add(1, SeqCst) + 1;
+			let output = String::from("buyers-checker-pool-");
+			output + &id.to_string()
+		})
+		.on_thread_stop(|| {
+			THREAD_ID.fetch_sub(1, SeqCst);
+		})
+		.build()
+		.into_diagnostic()?
+		.block_on(run())
+}
+
+async fn run() -> Result<()> {
+	Ok(())
 }
