1c7fdbc1ee91c4abb75798a98c4739f21283a0fe
obellish <obellish@mpilabels.com>
2024-03-14 10:24:13 -0400

    works

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ use tokio::{
 	runtime::Builder,
 };
 use tokio_stream::wrappers::ReadDirStream;
+use tracing::{event, Level};
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 static THREAD_ID: AtomicUsize = AtomicUsize::new(1);
 
@@ -33,20 +35,49 @@ fn main() -> Result<()> {
 }
 
 async fn run() -> Result<()> {
-	let args = Args::parse();
+	let log_fmt_layer = fmt::layer()
+		.pretty()
+		.with_ansi(true)
+		.with_thread_ids(true)
+		.with_thread_names(true)
+		.with_file(false)
+		.with_filter(EnvFilter::try_new("debug").into_diagnostic()?);
 
+	let output_path = "./output.log";
+
+	let output_file = std::fs::File::create(output_path).into_diagnostic()?;
+	let log_fs_layer = fmt::layer()
+		.with_ansi(false)
+		.compact()
+		.with_writer(output_file);
+
+	tracing_subscriber::registry()
+		.with(log_fmt_layer)
+		.with(log_fs_layer)
+		.try_init()
+		.into_diagnostic()?;
+
+	let args = Args::try_parse().into_diagnostic()?;
+
+	let mut futures = Vec::new();
 	if let Some(file) = args.file_path {
 		check_file(file).await?;
 	} else if let Some(folder) = args.folder_path {
-		check_folder(folder).await?;
+		check_directory(folder, &mut futures).await?;
 	} else {
 		panic!("No file or folder path was given.");
 	}
 
+	futures::future::try_join_all(futures).await?;
+
 	Ok(())
 }
 
-fn check_folder(path: PathBuf) -> BoxFuture<'static, Result<()>> {
+fn check_directory<'a>(
+	path: PathBuf,
+	futures: &'a mut Vec<BoxFuture<'static, Result<()>>>,
+) -> BoxFuture<'a, Result<()>> {
+	event!(Level::INFO, ?path, "checking directory");
 	assert!(path.is_dir());
 	async move {
 		let mut dir_stream = ReadDirStream::new(read_dir(path).await.into_diagnostic()?);
@@ -55,9 +86,9 @@ fn check_folder(path: PathBuf) -> BoxFuture<'static, Result<()>> {
 			let path = entry.path();
 
 			if path.is_file() {
-				check_file(path).await?;
+				futures.push(check_file(path));
 			} else if path.is_dir() {
-				check_folder(path).await?;
+				check_directory(path, futures).await?;
 			} else {
 				panic!("invalid path {} found", path.display());
 			}
@@ -68,41 +99,48 @@ fn check_folder(path: PathBuf) -> BoxFuture<'static, Result<()>> {
 	.boxed()
 }
 
-async fn check_file(path: PathBuf) -> Result<()> {
+fn check_file(path: PathBuf) -> BoxFuture<'static, Result<()>> {
 	assert!(path.is_file());
-	let file = File::open(path).await.into_diagnostic()?;
+	event!(Level::DEBUG, ?path, "checking file");
+	async move {
+		let file = File::open(path).await.into_diagnostic()?;
 
-	let mut reader = AsyncReaderBuilder::new()
-		.has_headers(false)
-		.create_reader(file);
+		let mut reader = AsyncReaderBuilder::new()
+			.has_headers(false)
+			.create_reader(file);
 
-	let mut record_stream = reader.records();
+		let mut record_stream = reader.records();
 
-	let mut records = Vec::new();
+		let mut records = Vec::new();
 
-	while let Some(item) = record_stream.try_next().await.into_diagnostic()? {
-		records.extend(
-			item.get(BARCODE_INDEX)
-				.map(|s| {
-					let mut out = s.to_owned();
-					out.pop();
-					out
-				})
-				.filter(|s| !s.is_empty()),
-		);
+		while let Some(item) = record_stream.try_next().await.into_diagnostic()? {
+			records.extend(
+				item.get(BARCODE_INDEX)
+					.map(|s| {
+						let mut out = s.to_owned();
+						out.pop();
+						out
+					})
+					.filter(|s| !s.is_empty()),
+			);
+		}
+
+		let cloned_records = records.clone();
+
+		records.sort();
+
+		assert_eq!(records.len(), cloned_records.len());
+
+		for i in 0..records.len() - 1 {
+			let original = cloned_records.get(i);
+			let sorted = records.get(i);
+			event!(Level::TRACE, ?original, ?sorted, %i, "matching records");
+			assert_eq!(original, sorted);
+		}
+
+		Ok(())
 	}
-
-	let cloned_records = records.clone();
-
-	records.sort();
-
-	assert_eq!(records.len(), cloned_records.len());
-
-	for i in 0..records.len() - 1 {
-		assert_eq!(records.get(i), cloned_records.get(i));
-	}
-
-	Ok(())
+	.boxed()
 }
 
 #[derive(Debug, Parser)]
