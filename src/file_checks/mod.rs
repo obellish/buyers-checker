2e3ff1a73d280f742c18a1c748c3deeb7c80141e
obellish <obellish@mpilabels.com>
2024-03-21 13:35:19 -0400

    can remove the boxed future

--- src/file_checks/mod.rs
+++ src/file_checks/mod.rs
@@ -3,10 +3,7 @@ mod error;
 use std::path::{Path, PathBuf};
 
 use csv_async::{AsyncReaderBuilder, AsyncWriterBuilder, StringRecord};
-use futures::{
-	future::{BoxFuture, FutureExt},
-	TryFutureExt, TryStreamExt as _,
-};
+use futures::{TryFutureExt, TryStreamExt as _};
 use tokio::fs::File;
 use tracing::{event, Level};
 
@@ -35,67 +32,64 @@ pub async fn check_directory(path: PathBuf, output_path: PathBuf) -> Result<(), 
 	Ok(())
 }
 
-pub fn check_file<'a>(
+pub async fn check_file<'a>(
 	input_path: &'a Path,
 	output_path: &'a Path,
-) -> BoxFuture<'a, Result<(), CheckFileError>> {
+) -> Result<(), CheckFileError> {
 	assert!(input_path.is_file());
 	event!(Level::DEBUG, ?input_path, "checking file");
-	async move {
-		let file = File::open(&input_path).await?;
+	let file = File::open(&input_path).await?;
 
-		let mut reader = AsyncReaderBuilder::new()
-			.has_headers(false)
-			.create_reader(file);
+	let mut reader = AsyncReaderBuilder::new()
+		.has_headers(false)
+		.create_reader(file);
 
-		let records = reader
-			.records()
-			.try_filter_map(|item| {
-				let index = item.get(0).and_then(|s| s.parse::<usize>().ok());
-				let barcode = item.get(4).and_then(|s| {
-					let mut out = s.to_owned();
-					out.pop();
-					out.parse::<u64>().ok()
-				});
+	let records = reader
+		.records()
+		.try_filter_map(|item| {
+			let index = item.get(0).and_then(|s| s.parse::<usize>().ok());
+			let barcode = item.get(4).and_then(|s| {
+				let mut out = s.to_owned();
+				out.pop();
+				out.parse::<u64>().ok()
+			});
 
-				futures::future::ok(index.zip(barcode))
-			})
-			.try_collect::<Vec<_>>()
-			.await?;
+			futures::future::ok(index.zip(barcode))
+		})
+		.try_collect::<Vec<_>>()
+		.await?;
 
-		let output_path = output_path.join(
-			input_path
-				.file_name()
-				.and_then(|s| s.to_str())
-				.unwrap_or("FAIL.txt"),
-		);
+	let output_path = output_path.join(
+		input_path
+			.file_name()
+			.and_then(|s| s.to_str())
+			.unwrap_or("FAIL.txt"),
+	);
 
-		let output_file = File::create(output_path);
+	let output_file = File::create(output_path);
 
-		let mut output_writer = AsyncWriterBuilder::new()
-			.flexible(false)
-			.has_headers(false)
-			.create_writer(output_file.await?);
+	let mut output_writer = AsyncWriterBuilder::new()
+		.flexible(false)
+		.has_headers(false)
+		.create_writer(output_file.await?);
 
-		for (i, (record_index, record)) in records.iter().copied().enumerate().skip(1) {
-			let Some((_, before)) = records.get(i - 1).copied() else {
-				continue;
-			};
+	for (i, (record_index, record)) in records.iter().copied().enumerate().skip(1) {
+		let Some((_, before)) = records.get(i - 1).copied() else {
+			continue;
+		};
 
-			if record - 1 != before {
-				event!(Level::ERROR, ?input_path, %record_index, %record);
+		if record - 1 != before {
+			event!(Level::ERROR, ?input_path, %record_index, %record);
 
-				let string_record = [record_index.to_string(), record.to_string()]
-					.into_iter()
-					.collect::<StringRecord>();
+			let string_record = [record_index.to_string(), record.to_string()]
+				.into_iter()
+				.collect::<StringRecord>();
 
-				let byte_record = string_record.into_byte_record();
+			let byte_record = string_record.into_byte_record();
 
-				output_writer.write_byte_record(&byte_record).await?;
-			}
+			output_writer.write_byte_record(&byte_record).await?;
 		}
-
-		Ok(())
 	}
-	.boxed()
+
+	Ok(())
 }
